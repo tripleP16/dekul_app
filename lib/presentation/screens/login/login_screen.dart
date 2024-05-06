cf72550561728c5c84e935bf1cfb9a197543e1c8
pablo perez <perez51160900@hotmail.com>
2024-05-06 23:26:49 +0200

    creating architecture and routes

---
+++ lib/presentation/screens/login/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String routeName = 'login';
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Login Screen'),
+      ),
+    );
+  }
+}
