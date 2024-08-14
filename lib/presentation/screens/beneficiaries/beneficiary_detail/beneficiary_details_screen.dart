2d11fd3edcb4ef9cbaa705bfce1dddce13454ba9
pablo perez <perez51160900@hotmail.com>
2024-08-14 16:40:54 +0200

    update permissions

--- lib/presentation/screens/beneficiaries/beneficiary_detail/beneficiary_details_screen.dart
+++ lib/presentation/screens/beneficiaries/beneficiary_detail/beneficiary_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:communitary_service_app/config/helpers/locator.dart';
+import 'package:communitary_service_app/config/services/contracts/permissions_service.dart';
+import 'package:communitary_service_app/config/services/models/permissions_enum.dart';
 import 'package:communitary_service_app/domain/repositories/beneficiaries/beneficiaries_repository.dart';
 import 'package:communitary_service_app/presentation/blocs/beneficiaries/beneficiary_detail/beneficiary_detail_cubit.dart';
 import 'package:communitary_service_app/presentation/blocs/beneficiaries/beneficiary_detail/beneficiary_detail_state.dart';
@@ -19,18 +21,25 @@ class BeneficiaryDetailsScreen extends StatelessWidget {
         getIt<BeneficiariesRepository>(),
         id,
       ),
-      child: Scaffold(
-        appBar: CustomAppBar(
-          title: 'Beneficiario',
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.edit),
-            ),
-          ],
-        ),
-        body: const _Body(),
-      ),
+      child: FutureBuilder(
+          future: getIt<PermissionsService>()
+              .hasAccess(PermissionsEnum.updateBeneficiary),
+          builder: (context, snapshot) {
+            return Scaffold(
+              appBar: CustomAppBar(
+                title: 'Beneficiario',
+                actions: snapshot.data ?? false
+                    ? [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.edit),
+                        ),
+                      ]
+                    : null,
+              ),
+              body: const _Body(),
+            );
+          }),
     );
   }
 }
